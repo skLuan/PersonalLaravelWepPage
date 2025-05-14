2a2034e9fc125a306af589924e19a22745861673
skLuan <luan.erazo@skyrocket.com.co>
2025-05-14 17:14:02 -0500

    added the card project component and changes in the general css

---
+++ resources/views/components/card/project.blade.php
@@ -0,0 +1,20 @@
+<div class="!border-skl-purple border p-2 rounded-lg">
+    <figure>
+        <picture>
+            <source media="(min-width: )" srcset=""><img src="" alt="">
+        </picture>
+    </figure>
+    <p class="flex flex-row items-center"> <span class="ml-auto">Dr Carlos Gamboa page</span> <iconify-icon icon="mingcute:right-fill" width="24" height="24"></iconify-icon></p>
+    <div class="w-full flex justify-between items-center"><h5 class="opacity-50 font-skl-titles">Url</h5><a
+            href="https://drcarlosgamboa.com/">https://drcarlosgamboa.com/</a></div>
+    <div class="flex justify-between py-2 items-center"><h5 class="opacity-50 font-skl-titles">Skills</h5>
+        <div class="flex flex-row justify-evenly w-full">
+            <span class="pillskl !border-skl-purple border rounded-full px-3 py-1">Front dev</span>
+            <span class="pillskl !border-skl-purple border rounded-full px-3 py-1">Ux/Ui</span>
+        </div>
+    </div>
+    <p class="border-t !border-t-skl-purple">
+        I am a self-taught digital creator with over 4 years of experience in web design and development,
+        passionate about continuous learning and innovation.
+    </p>
+</div>
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -108,26 +108,8 @@
             <li><button>Graphic Design</button></li>
         </ul>
         <ul>
-            <li class="!border-skl-purple border p-2 rounded-lg">
-                <figure>
-                    <picture>
-                        <source media="(min-width: )" srcset=""><img src="" alt="">
-                    </picture>
-                </figure>
-                <p>Dr Carlos Gamboa page <iconify-icon icon="mingcute:right-fill" width="24"
-                        height="24"></iconify-icon></p>
-                <div class="w-full flex justify-between"><span class="font-skl-titles">Url</span><a
-                        href="https://drcarlosgamboa.com/">https://drcarlosgamboa.com/</a></div>
-                <div class="flex justify-between py-2"><span class="font-skl-titles">Skills</span>
-                    <div class="flex flex-row justify-evenly w-full">
-                        <span class="pillskl !border-skl-purple border rounded-full px-3 py-1">Front dev</span>
-                        <span class="pillskl !border-skl-purple border rounded-full px-3 py-1">Ux/Ui</span>
-                    </div>
-                </div>
-                <p class="border-t !border-t-skl-purple">
-                    I am a self-taught digital creator with over 4 years of experience in web design and development,
-                    passionate about continuous learning and innovation.
-                </p>
+            <li>
+                <x-card.project />
             </li>
         </ul>
     </section>
