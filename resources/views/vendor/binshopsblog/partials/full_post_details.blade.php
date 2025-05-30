d406373bc5a28e11f651d4094f140367e2b99ea6
skLuan <luan.erazo@skyrocket.com.co>
2025-05-29 21:43:11 -0500

    fixing the desktop view of single post

--- resources/views/vendor/binshopsblog/partials/full_post_details.blade.php
+++ resources/views/vendor/binshopsblog/partials/full_post_details.blade.php
@@ -2,28 +2,37 @@
     <a href="{{$post->edit_url()}}" class="btn btn-outline-secondary btn-sm pull-right float-right">Edit
         Post</a>
 @endif
+<section class="blog_post_header relative flex flex-col md:flex-row items-center gap-4">
+    <div class="w-6/12 absolute top-0 p-4 mt-36 text-shadow-md bg-skl-black-50">
+        <h1 class='blog_title'>{{$post->title}}</h1>
+        <h5 class='blog_subtitle'>{{$post->subtitle}}</h5>
+    </div>
+    <figure class="overflow-hidden ml-auto relative rounded-sm -z-10 w-7/12 right-0">
+        <picture>
+            <?=$post->image_tag("large", false, 'w-full'); ?>
+        </picture>
+    </figure>
+</section>
 
-<h1 class='blog_title'>{{$post->title}}</h1>
-<h5 class='blog_subtitle'>{{$post->subtitle}}</h5>
-
-
-<?=$post->image_tag("medium", false, 'd-block mx-auto'); ?>
-
-<p class="blog_body_content">
-    {!! $post->post_body_output() !!}
-
-    {{--@if(config("binshopsblog.use_custom_view_files")  && $post->use_view_file)--}}
-    {{--                                // use a custom blade file for the output of those blog post--}}
-    {{--   @include("binshopsblog::partials.use_view_file")--}}
-    {{--@else--}}
-    {{--   {!! $post->post_body !!}        // unsafe, echoing the plain html/js--}}
-    {{--   {{ $post->post_body }}          // for safe escaping --}}
-    {{--@endif--}}
-</p>
-
-<hr/>
-
-Posted <strong>{{$post->posted_at->diffForHumans()}}</strong>
-
-@includeWhen($post->author,"binshopsblog::partials.author",['post'=>$post])
-@includeWhen($post->categories,"binshopsblog::partials.categories",['post'=>$post])
+<section>
+    <article class="max-w-screen-md">
+        <p class="blog_body_content">
+            {!! $post->post_body_output() !!}
+        
+            {{--@if(config("binshopsblog.use_custom_view_files")  && $post->use_view_file)--}}
+            {{--                                // use a custom blade file for the output of those blog post--}}
+            {{--   @include("binshopsblog::partials.use_view_file")--}}
+            {{--@else--}}
+            {{--   {!! $post->post_body !!}        // unsafe, echoing the plain html/js--}}
+            {{--   {{ $post->post_body }}          // for safe escaping --}}
+            {{--@endif--}}
+        </p>
+        
+        <hr/>
+        
+        Posted <strong>{{$post->posted_at->diffForHumans()}}</strong>
+        
+        @includeWhen($post->author,"binshopsblog::partials.author",['post'=>$post])
+        @includeWhen($post->categories,"binshopsblog::partials.categories",['post'=>$post])
+    </article>
+</section>
--- resources/views/vendor/binshopsblog/single_post.blade.php
+++ resources/views/vendor/binshopsblog/single_post.blade.php
@@ -1,42 +1,40 @@
-@extends("layouts.app",['title'=>$post->gen_seo_title()])
+@extends('layouts.app', ['title' => $post->gen_seo_title()])
 
 @section('blog-custom-css')
-    <link type="text/css" href="{{ asset('binshops-blog.css') }}" rel="stylesheet">
+    {{-- <link type="text/css" href="{{ asset('binshops-blog.css') }}" rel="stylesheet"> --}}
 @endsection
 
-@section("content")
-
-    @if(config("binshopsblog.reading_progress_bar"))
-        <div id="scrollbar">
-            <div id="scrollbar-bg"></div>
-        </div>
-    @endif
-
-
-    <div class='container'>
-    <div class='row'>
-        <div class='col-sm-12 col-md-12 col-lg-12'>
-
-            @include("binshopsblog::partials.show_errors")
-            @include("binshopsblog::partials.full_post_details")
+@section('content')
+    <div class='pt-24 w-full'>
+        @if (config('binshopsblog.reading_progress_bar'))
+            <div id="scrollbar">
+                <div id="scrollbar-bg"></div>
+            </div>
+        @endif
 
 
-            @if(config("binshopsblog.comments.type_of_comments_to_show","built_in") !== 'disabled')
-                <div class="" id='maincommentscontainer'>
-                    <h2 class='text-center' id='BinshopsBlogcomments'>Comments</h2>
-                    @include("binshopsblog::partials.show_comments")
-                </div>
-            @else
-                {{--Comments are disabled--}}
-            @endif
+        <div class='row'>
+            <div class=''>
+
+                @include('binshopsblog::partials.show_errors')
+                @include('binshopsblog::partials.full_post_details')
 
 
+                @if (config('binshopsblog.comments.type_of_comments_to_show', 'built_in') !== 'disabled')
+                    <div class="" id='maincommentscontainer'>
+                        <h2 class='text-center' id='BinshopsBlogcomments'>Comments</h2>
+                        @include('binshopsblog::partials.show_comments')
+                    </div>
+                @else
+                    {{-- Comments are disabled --}}
+                @endif
+
+
+            </div>
         </div>
     </div>
-    </div>
-
 @endsection
 
 @section('blog-custom-js')
-    <script src="{{asset('binshops-blog.js')}}"></script>
+    <script src="{{ asset('binshops-blog.js') }}"></script>
 @endsection
