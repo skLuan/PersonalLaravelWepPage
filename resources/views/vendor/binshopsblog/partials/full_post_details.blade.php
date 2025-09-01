3a3e1be3e9e5cc44ec3ed4046a696e105a50b3f5
skLuan <luan.erazo@skyrocket.com.co>
2025-08-31 19:48:05 -0500

    refactor full_post_details layout for improved responsiveness; update Vite config to use CSS instead of SASS

--- resources/views/vendor/binshopsblog/partials/full_post_details.blade.php
+++ resources/views/vendor/binshopsblog/partials/full_post_details.blade.php
@@ -2,20 +2,20 @@
     <a href="{{ $post->edit_url() }}" class="btn btn-outline-secondary btn-sm pull-right float-right">Edit
         Post</a>
 @endif
-<section class="blog_post_header relative flex flex-col-reverse lg:flex-row items-end gap-4 mb-12">
-    <div class="lg:w-6/12 top-0 lg:mt-16 text-shadow-md bg-skl-black-50">
-        <h1 class='blog_title'>{{ $post->title }}</h1>
-        <h5 class='blog_subtitle'>{{ $post->subtitle }}</h5>
+<section class="blog_post_header relative mb-12 lg:w-7/12 lg:mx-auto">
+    <h1 class='blog_title lg:mt-16 text-shadow-md bg-skl-black-50 w-full'>{{ $post->title }}</h1>
+    <div class="flex flex-col lg:flex-row items-center">
+        <h5 class='blog_subtitle text-shadow-md bg-skl-black-50 w-full mb-12 lg:mb-0'>{{ $post->subtitle }}</h5>
+        <figure class="overflow-hidden md:h-80 lg:h-auto lg:ml-auto relative rounded-sm -z-10 lg:w-7/12 right-0 bg-gray-200">
+            <picture>
+                <?= $post->image_tag('large', false, 'w-full md:-translate-y-1/2 lg:translate-y-0') ?>
+            </picture>
+        </figure>
     </div>
-    <figure class="overflow-hidden md:h-80 lg:h-auto lg:ml-auto relative rounded-sm -z-10 lg:w-7/12 right-0">
-        <picture>
-            <?= $post->image_tag('large', false, 'w-full md:-translate-y-1/2 lg:translate-y-0') ?>
-        </picture>
-    </figure>
 </section>
 
 <section>
-    <article class="lg:w-7/12 lg:ml-6">
+    <article class="lg:w-7/12 lg:mx-auto">
         <p class="blog_body_content">
             {!! $post->post_body_output() !!}
 
