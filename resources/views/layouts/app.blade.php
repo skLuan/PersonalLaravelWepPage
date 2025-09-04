9a02f4c88c3b39a88b9976ab874edcdb27f33159
skLuan <luan.erazo@skyrocket.com.co>
2025-09-04 04:27:55 +0000

    add the microsoft clarity code in the app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,6 +35,14 @@
 
     gtag('config', '{{ env('GOOGLE_TAG_ID') }}');
 </script>
+<!-- clarity tag -->
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "t5bhdus54g");
+</script>
 
 <body class="antialiased bg-skl-black font-skl-nunito">
     <div id="app" class="relative">
